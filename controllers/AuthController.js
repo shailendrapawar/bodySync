63e303eb519900203aa503689772d26b3b15bd17
shailendrapawar <shailendrapawar980@gmail.com>
2024-08-11 19:41:26 +0530

    added:login  functionality

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,9 +2,38 @@ const AuthModel = require("../models/AuthModels")
 const bcrypt = require("bcrypt");
 
 class AuthController {
+    //============login functionlity==============================
     static login = async (req, res) => {
-        res.send(req.body.test)
+       
+        const{email, password}=req.body;
+        let isUser=await AuthModel.findOne({email:email});
+        if(!isUser){
+            res.send({
+                msg:"user not registered",
+                status:400
+            })
+        }else{
+           
+            let isValid= await bcrypt.compare(password,isUser.password);
+            console.log(isValid);
+            if(isValid){
+                res.send({
+                    msg:"authentic user",
+                    status:200,
+                    userId:isUser._id
+                })
+            }else{
+                res.send({
+                    msg:"wrong credentials",
+                    status:400,
+                    userId:null
+                })
+            }
+        }
     }
+
+
+    //============register functionality===========================
     static register = async (req, res) => {
         const { name, email, password, gender } = req.body;
 
