c614b4dd3bd56995efc8841865c266d0fc8e30d9
shailendrapawar <shailendrapawar980@gmail.com>
2024-08-11 18:59:10 +0530

    added:register functionality

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,13 +1,52 @@
+const AuthModel = require("../models/AuthModels")
+const bcrypt = require("bcrypt");
 
+class AuthController {
+    static login = async (req, res) => {
+        res.send(req.body.test)
+    }
+    static register = async (req, res) => {
+        const { name, email, password, gender } = req.body;
 
-class AuthController{
-    static login=async(req,res)=>{
-        res.send(req.body.test)    }
-    static register=async(req,res)=>{
-        res.send(req.body.test)    }
+        let isExist = await AuthModel.findOne({ email, email }).select("password");
 
-    static changePassword=async(req,res)=>{
-        res.send(req.body.test)    }
+        if (isExist) {
+            res.send({
+                status: 400,
+                msg: "user already exist"
+            })
+        } else {
+            let salt = await bcrypt.genSalt(10);
+            const hashPass = await bcrypt.hash(password, salt);
+
+            let userData = new AuthModel({
+                name: name,
+                email: email,
+                password: hashPass,
+                gender: gender
+            })
+
+            let isCreated =await  userData.save();
+
+            if (isCreated) {
+                res.send({
+                    msg: "user created",
+                    status: 201
+                })
+            } else {
+                res.send({
+                    msg: "user not created",
+                    status: 400
+                })
+            }
+
+        }
+
+    }
+
+    static changePassword = async (req, res) => {
+        res.send(req.body.test)
+    }
 }
 
-module.exports=AuthController;
+module.exports = AuthController;
