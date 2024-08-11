9afb83f80490204038ea75e8716583a03b47a40c
shailendrapawar <shailendrapawar980@gmail.com>
2024-08-11 17:22:29 +0530

    added: db connection established

---
+++ config/dbConfig.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const dbConnect = async () => {
+    try {
+        const dbRes = await mongoose.connect(process.env.MONGO_URL);
+        if (dbRes) {
+            console.log("DB conncted");
+        } else {
+            console.log("connection error")
+        }
+    }catch(err){
+        console.log("errror:"+err);
+    }
+
+}
+
+module.exports=dbConnect;
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express=require("express")
 const app=express()
 const cors=require("cors");
+require("dotenv").config();
+
+//db connection---------------
+const dbConnect=require("./config/dbConfig")
+dbConnect();
 
 app.use(cors(
     {
         origin:"*"
     }
 ))
+
+
 app.get("/",(req,res)=>{
     res.send("working");
 })
